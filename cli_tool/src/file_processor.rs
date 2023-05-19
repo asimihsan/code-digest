f1d1faf52b7f1435a14d074a04423d9f799faea7
Asim Ihsan <ssh@sshemail.ihsan.io>
2023-05-18 22:08:20 -0700

    WIP refactor cli

---
+++ cli_tool/src/file_processor.rs
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2023 Asim Ihsan.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+use file_system::GlobPatternMatcher;
+use language_parsers::{parse, ParseConfig};
+use std::path::Path;
+
+pub fn process_file(
+    file_path: &Path,
+    go_config: &ParseConfig,
+    rust_config: &ParseConfig,
+    glob_matcher: &GlobPatternMatcher,
+    mut callback: impl FnMut(&str),
+) {
+    if glob_matcher.matches(file_path) {
+        callback(&format!("`{}`", file_path.display()));
+        let source_code = std::fs::read_to_string(file_path).expect("Unable to read file");
+        callback(&format!("```\n{}\n```\n", source_code));
+        return;
+    }
+
+    let source_code = std::fs::read_to_string(file_path).expect("Unable to read file");
+
+    let extension = file_path.extension();
+    if extension.is_none() {
+        return;
+    }
+    let extension = extension.unwrap().to_str().unwrap();
+    let result = match extension {
+        "go" => parse(&source_code, go_config).unwrap_or_else(|e| {
+            eprintln!("Error parsing file: {}", e);
+            std::process::exit(1);
+        }),
+        "rs" => parse(&source_code, rust_config).unwrap_or_else(|e| {
+            eprintln!("Error parsing file: {}", e);
+            std::process::exit(1);
+        }),
+        _ => {
+            return;
+        }
+    };
+
+    callback(&format!("`{}`", file_path.display()));
+    match extension {
+        "go" => {
+            callback("```go\n");
+        }
+        "rs" => {
+            callback("```rust\n");
+        }
+        _ => {
+            eprintln!("Unknown extension: {}", extension);
+            std::process::exit(1);
+        }
+    }
+
+    for (i, key_content) in result.iter().enumerate() {
+        callback(&key_content.content.to_string());
+        if i < result.len() - 1 {
+            callback("\n");
+        }
+    }
+    callback("```\n");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::GlobPatternMatcher;
+    use language_parsers::{default_parse_config_for_language, Language};
+    use std::io::Write;
+
+    #[test]
+    fn test_process_file_rust() {
+        let rust_config = default_parse_config_for_language(Language::Rust);
+        let go_config = default_parse_config_for_language(Language::Go);
+        let glob_matcher = GlobPatternMatcher::new_from_strings(vec![]).unwrap();
+
+        // Create a temporary file with Rust code
+        let temp_dir = tempfile::tempdir().unwrap();
+        let file_path = temp_dir.path().join("test.rs");
+        std::fs::write(
+            &file_path,
+            r#"
+fn main() {
+    println!("Hello, world!");
+}
+"#,
+        )
+        .unwrap();
+
+        let mut actual_output = String::new();
+        process_file(&file_path, &go_config, &rust_config, &glob_matcher, |s| {
+            actual_output.push_str(s);
+            actual_output.push_str("\n");
+        });
+
+        let expected_output = r#"
+fn main() {
+    // ...
+}
+        "#;
+
+        // TODO compare, remember filename
+    }
+}
--- cli_tool/src/main.rs
+++ cli_tool/src/main.rs
@@ -8,15 +8,17 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
+mod file_processor;
+
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-use std::collections::HashSet;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
+use crate::file_processor::process_file;
 use clap::Parser;
 use file_system::{get_files, GlobPatternMatcher};
-use language_parsers::{default_parse_config_for_language, parse};
+use language_parsers::{default_parse_config_for_language, parse, ParseConfig};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -35,7 +37,7 @@ struct Cli {
 
 pub fn main() {
     let cli = Cli::parse();
-    if cli.directory.is_dir() == false {
+    if !cli.directory.is_dir() {
         eprintln!("Not a directory: {}", cli.directory.display());
         std::process::exit(1);
     }
@@ -54,60 +56,13 @@ pub fn main() {
         .collect();
     let glob_matcher = GlobPatternMatcher::new_from_strings(cli.include).unwrap();
 
-    let files = get_files(directory.clone(), &ignore_dirs);
+    let files = get_files(directory, &ignore_dirs);
     let go_config = default_parse_config_for_language(language_parsers::Language::Go);
     let rust_config = default_parse_config_for_language(language_parsers::Language::Rust);
 
-    for (file_number, file_path) in files.iter().enumerate() {
-        if glob_matcher.matches(file_path) {
-            println!("`{}`", file_path.display());
-            let source_code = std::fs::read_to_string(&file_path).expect("Unable to read file");
-            println!("```\n{}\n```\n", source_code);
-            continue;
-        }
-
-        let source_code = std::fs::read_to_string(&file_path).expect("Unable to read file");
-
-        let extension = file_path.extension();
-        if extension.is_none() {
-            continue;
-        }
-        let extension = extension.unwrap().to_str().unwrap();
-        let result = match extension {
-            "go" => parse(&source_code, &go_config).unwrap_or_else(|e| {
-                eprintln!("Error parsing file: {}", e);
-                std::process::exit(1);
-            }),
-            "rs" => parse(&source_code, &rust_config).unwrap_or_else(|e| {
-                eprintln!("Error parsing file: {}", e);
-                std::process::exit(1);
-            }),
-            _ => continue,
-        };
-
-        println!("`{}`", file_path.display());
-        match extension {
-            "go" => {
-                println!("```go\n");
-            }
-            "rs" => {
-                println!("```rust\n");
-            }
-            _ => {
-                eprintln!("Unknown extension: {}", extension);
-                std::process::exit(1);
-            }
-        }
-
-        for (i, key_content) in result.iter().enumerate() {
-            println!("{}", key_content.content);
-            if i < result.len() - 1 {
-                println!();
-            }
-        }
-        println!("```\n");
-        if file_number < files.len() - 1 {
-            println!();
-        }
+    for file_path in files {
+        process_file(&file_path, &go_config, &rust_config, &glob_matcher, |s| {
+            println!("{}", s);
+        });
     }
 }
